9a1e3ff4405d494dd555efb59248f354450b2c1b
shivam2534 <134926520+shivamkanchole@users.noreply.github.com>
2024-03-28 17:53:04 +0530

    First commit

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className=" text-red-700 text-center text-2xl"> 
-      Wellcome to the Next Generation
+      Wellcome to Page 2 of this site
     </div>
   );
 }
